ebedd0d7ac90a6fd16617db2c6c36a7a416c4e61
Ang543 <88729095+Ang543@users.noreply.github.com>
2021-10-17 13:27:15 -0400

    fingers crossed its not broken in some way im not aware of

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,65 @@
-// Assignment code here
+var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+  "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var lowerCaseArray = [abcdefghijklmnopqrstuvwyz];
+var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+  "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-var upperCaseArray = [ABCDEFGHIJKLMNOPQRSTUVWXYZ];
+var specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "<", ">", ";", ":", "{", "}", "[", "]", "?", "~"];
 
-var specialCharacter = [!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
+var numericCharacter = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-var numericCharacter = [0123456789];
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+  var finalPassword = ""
+
+  var passwordSize = prompt("Choose a number between 8 and 128 for the length of your password.");
+  passwordSize = parseInt(passwordSize);
+  if (passwordSize >= 8 && passwordSize <= 128) {
+    var isNumeric = confirm("Do you want numbers?");
+    var isLowercase = confirm("Do you want lowercase letters?");
+    var isUppercase = confirm("Do you want uppercase letters?");
+    var isSpecial = confirm("Do you want special characters?");
+
+    if (isLowercase === false && isUppercase === false && isSpecial === false && isNumeric === false) {
+      alert("You must choose at least one.");
+      return "";
+    }
+
+    for (let i = 0; i < passwordSize && finalPassword.length < passwordSize; i++) {
+      
+      if (isNumeric && finalPassword.length < passwordSize) {
+        var index = Math.floor(Math.random() * numericCharacter.length);
+        finalPassword = finalPassword + numericCharacter[index];
+      }
+      
+      if (isLowercase && finalPassword.length < passwordSize) {
+        var index = Math.floor(Math.random() * lowerCaseArray.length);
+        finalPassword = finalPassword + lowerCaseArray[index];
+      }
+
+      if (isUppercase && finalPassword.length < passwordSize) {
+        var index = Math.floor(Math.random() * upperCaseArray.length);
+        finalPassword = finalPassword + upperCaseArray[index];
+      }
+
+      if (isSpecial && finalPassword.length < passwordSize) {
+        var index = Math.floor(Math.random() * specialCharacter.length);
+        finalPassword = finalPassword + specialCharacter[index];
+      }
+    }
+  }
+
+  else {
+    alert("Try again.")
+  }
+
+  return finalPassword;
+};
